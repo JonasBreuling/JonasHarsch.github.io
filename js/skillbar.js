bca8fb4ab3f92e3bae4eda87ab025cf62834a41d
JonasHarsch <jonas_harsch@web.de>
2018-03-20 15:00:38 +0100

    Added animated skillbars.

---
+++ js/skillbar.js
@@ -0,0 +1,19 @@
+// see https://stackoverflow.com/questions/21561480/trigger-event-when-user-scroll-to-specific-element-with-jquery
+// for infomration about registrating when skill bars are reached
+
+ $(window).scroll(function() {
+    var hT = $('#skill-bar-wrapper').offset().top,
+        hH = $('#skill-bar-wrapper').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    // if (wS > (hT+hH-wH)){
+    if (wS > (hT+hH-1.4*wH)){
+        jQuery(document).ready(function(){
+            jQuery('.skillbar-container').each(function(){
+                jQuery(this).find('.skills').animate({
+                    width:jQuery(this).attr('data-percent')
+                },6000);
+            });
+        });
+    }
+ });
