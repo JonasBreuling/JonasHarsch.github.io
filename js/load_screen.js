d47598cad0b73e0281cb00e20e73a6f9cd1cebd6
JonasHarsch <jonas_harsch@web.de>
2018-04-03 11:00:27 +0200

    More delay and fadeOut time.

--- js/load_screen.js
+++ js/load_screen.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
     // var load_screen = document.getElementById("load_screen");
     // this.document.body.removeChild(load_screen);
     // jQuery('.loading-page-bg').delay(3000).fadeOut(500);
-    jQuery('.loading-page-bg').delay(500).fadeOut(500);
+    jQuery('.loading-page-bg').delay(1000).fadeOut(1000);
 });
 
 // var readyStateCheckInterval = setInterval(function() {
