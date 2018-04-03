0698f386ab8fe5f31f9b9a1b09b0d67a964a0263
JonasHarsch <jonas_harsch@web.de>
2018-04-03 09:25:50 +0200

    Added pageloader

---
+++ js/load_screen.js
@@ -0,0 +1,47 @@
+// window.addEventListener("load", function() {
+//     var load_screen = document.getElementById("load_screen");
+//     this.document.body.removeChild(load_screen);
+// });
+
+// var readyStateCheckInterval = setInterval(function() {
+//     if (document.readyState === "complete") {
+//         clearInterval(readyStateCheckInterval);
+//         var load_screen = document.getElementById("load_screen");
+//         this.document.body.removeChild(load_screen);
+//     }
+// }, 10);
+
+/*
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'complete') {
+        setTimeout(function(){
+            document.getElementById('interactive');
+            // document.getElementById('load').style.visibility="hidden";
+            jQuery('#load').delay(1000).fadeOut(1000);
+        }, 4000);
+    }
+}
+*/
+
+// jQuery(document).ready(function() {
+$(document).ready(function() {
+    jQuery('#load').delay(1000).fadeOut(500);
+});
+
+$(document).ready(function() {
+  
+    var counter = 0;
+    var c = 0;
+    var i = setInterval(function(){
+        $(".loading-page .counter h1").html(c + "%");
+        $(".loading-page .counter hr").css("width", c + "%");
+      
+      counter++;
+      c++;
+        
+      if(counter == 101) {
+          clearInterval(i);
+      }
+    }, 5);
+  });
